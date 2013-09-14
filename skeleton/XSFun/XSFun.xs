086c60f41db32e42b4a8cc6d6556463b34e75867
Sawyer X <xsawyerx@cpan.org>
2013-09-14 16:11:38 +0200

    clean up skeleton

--- skeleton/XSFun/XSFun.xs
+++ skeleton/XSFun/XSFun.xs
@@ -1,7 +1,6 @@
 #include "EXTERN.h"
 #include "perl.h"
 #include "XSUB.h"
-
 #include "ppport.h"
 
 /* Your C functions if any go here */
--- skeleton/XSFun/lib/XSFun.pm
+++ skeleton/XSFun/lib/XSFun.pm
@@ -1,21 +1,14 @@
 package XSFun;
 
-use 5.008005;
 use strict;
 use warnings;
+use Exporter;
+use XSLoader;
 
-require Exporter;
+our $VERSION     = '0.001';
+our %EXPORT_TAGS = ( 'all' => [] );
+our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 
-our @ISA = qw(Exporter);
-
-our %EXPORT_TAGS = ( 'all' => [ qw(
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our $VERSION = '0.01';
-
-require XSLoader;
 XSLoader::load('XSFun', $VERSION);
 
 1;
